0b20380b2e26243d776b2424911763d520350d1d
Raleigh Littles <raleighlittles@gmail.com>
2017-10-21 02:02:39 -0700

    First draft of calculation methods

---
+++ calculation.py
@@ -0,0 +1,18 @@
+import geopy.distance
+import math
+
+def euclidean_distance(lat_A, lon_A, lat_B, lon_B):
+    return math.sqrt( (lat_B - lat_A)**2 + (lon_B - lon_A)**2 )
+
+def great_circle_distance(lat_A, lon_A, lat_B, lon_B):
+    return geopy.distance.great_circle((lat_A, lon_A), (lon_B, lon_B))
+
+def vincenty_distance(lat_A, lon_A, lat_B, lon_B):
+    return geopy.distance.vincenty( (lat_A, lon_A), (lon_B, lon_B ))
+
+def compute_percentage_error(euclid, greatcircle, vincenty):
+    euclid_vicenty_error = percent_error(euclid, vincenty)
+    greatcircle_vincenty_error = percent_error(greatcircle, vincenty)
+
+def percent_error(measured, expected):
+    return round(((math.fabs(measured - expected) / expected) * 100), 3)
