fde9169d542dc31cd36aea86d48c0f1634498b9b
Raleigh Littles <raleighlittles@gmail.com>
2017-10-28 05:04:46 -0700

    Improved styling, added in better table rendering, incorporated error measurements, added rounding

--- calculation.py
+++ calculation.py
@@ -7,16 +7,18 @@ def euclidean_distance(lat_A, lon_A, lat_B, lon_B):
     return math.sqrt( (lat_B - lat_A)**2 + (lon_B - lon_A)**2 )
 
 def great_circle_distance(lat_A, lon_A, lat_B, lon_B):
-    return geopy.distance.great_circle((lat_A, lon_A), (lon_B, lon_B))
+    return round( geopy.distance.great_circle((lat_A, lon_A), (lon_B, lon_B)).km, 4)
 
 def vincenty_distance(lat_A, lon_A, lat_B, lon_B):
-    return geopy.distance.vincenty( (lat_A, lon_A), (lon_B, lon_B ))
+    return round( geopy.distance.vincenty( (lat_A, lon_A), (lon_B, lon_B )).km, 4)
 
 def compute_percentage_error(euclid, greatcircle, vincenty):
     euclid_vicenty_error = percent_error(euclid, vincenty)
+    print (euclid_vicenty_error)
     greatcircle_vincenty_error = percent_error(greatcircle, vincenty)
+    print (greatcircle_vincenty_error)
     return [euclid_vicenty_error, greatcircle_vincenty_error]
 
 def percent_error(measured, expected):
-    return round(((math.fabs(measured - expected) / expected) * 100), 3)
+    return round((math.fabs((measured - expected) / expected) * 100), 3)
 
